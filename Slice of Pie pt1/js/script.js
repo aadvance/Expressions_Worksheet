1ff45f9f555d82ac69a92ef879db16a0e150ffbf
aadvance <adouglas@fullsail.edu>
2015-02-12 11:38:33 -0800

    Slice of Pie setup
    created variables for people, pizza size, pizzas ordered

--- Slice of Pie pt1/js/script.js
+++ Slice of Pie pt1/js/script.js
@@ -1,4 +1,9 @@
 //Alfredo Douglas
-//February 11, 2015
+//February 12, 2015
 //Expressions Worksheet_Slice of Pie pt1
+
+//List variables for number of people at party, how many slices per pizza, how many pizzas ordered
+var peopleParty;
+var pizzaSlice;
+var pizzaOrdered;
 
