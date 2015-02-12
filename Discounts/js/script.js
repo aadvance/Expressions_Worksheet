41b5c5f1e9c56c64db8a821fb55520be260740b2
aadvance <adouglas@fullsail.edu>
2015-02-12 15:23:53 -0800

    added variables
    variables added for item, value, discount percent, and sales tax

--- Discounts/js/script.js
+++ Discounts/js/script.js
@@ -1,3 +1,11 @@
-/**
- * Created by alfredodouglas on 2/12/15.
+/*
+ Alfredo Douglas
+ February 12, 2015
+ Expressions Worksheet_Average Shopping Bill
  */
+
+//Creating variables for givens
+var priceOrig = 124.99;
+var discPercent = 25;
+var item = "cell phone";
+var salesTax = 9
