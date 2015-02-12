8c14d226fc268b2bbb30848b036401c191ba39b7
aadvance <adouglas@fullsail.edu>
2015-02-12 15:05:23 -0800

    code for average
    added code to determine average weekly spending based on total amounts
    added together

--- Average Shopping Bill/js/script.js
+++ Average Shopping Bill/js/script.js
@@ -4,6 +4,13 @@
  Expressions Worksheet_Average Shopping Bill
  */
 
-//Create array of different values for shopping totals
+//Create array of different values for shopping totals over 5 weeks
 var shoppingBills = [84, 75, 120, 95, 104];
 
+//Total amount spent over those 5 weeks
+var totalShopping = shoppingBills[0] + shoppingBills[1] + shoppingBills[2] + shoppingBills[3] + shoppingBills[4];
+//console.log(totalShopping)
+
+//Find average spent weekly of the total amount
+var averageWeekly = totalShopping / 5
+console.log(averageWeekly)
