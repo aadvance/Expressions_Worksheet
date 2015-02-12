33490317f798cb7b4b142159cfd547bb2305da8c
aadvance <adouglas@fullsail.edu>
2015-02-12 12:51:18 -0800

    Slice of Pizza pt2
    new project listing new info for Sparky’s share

---
+++ Slice of Pizza pt2/js/script.js
@@ -0,0 +1,13 @@
+//Alfredo Douglas
+//February 12, 2015
+//Expressions Worksheet_Slice of Pie pt2
+
+//List variables for number of people at party, how many slices per pizza, how many pizzas ordered
+var peopleParty;
+peopleParty = 10;
+var pizzaSlice;
+pizzaSlice = 8;
+var pizzaOrdered;
+pizzaOrdered =4;
+
+//Adding extra slices for Sparky
