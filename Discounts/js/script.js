44c2dde0cdba9693b1c4fb369e1c6ec6e5173721
aadvance <adouglas@fullsail.edu>
2015-02-12 16:51:33 -0800

    Final language
    updated console print with final language

--- Discounts/js/script.js
+++ Discounts/js/script.js
@@ -5,13 +5,16 @@
  */
 
 //Creating variables for givens
-var price = 125;
-var disc = 20;
+var price = 175;
+var disc = 25;
 var item = "cell phone";
-var tax = 10;
+var tax = 9.5;
 
 //Variables as results
-var total = price - (price * disc / 100)
-//console.log(taxNo)
-var totalTax = price - (price * disc / 100) + (total * tax / 100)
-//console.log(totalTax)
+var total = price - (price * disc / 100);
+//console.log(taxNo);
+var totalTax = price - (price * disc / 100) + (total * tax / 100);
+//console.log(totalTax);
+
+//Final language
+console.log("Your " + item + " was originally $" + price + "," + " but after a " + disc + "% discount, it is now $" + total + " without tax, and $" + totalTax + " with tax.");
